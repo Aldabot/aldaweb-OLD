15a896be2f13e78ec4e1b6784c43c1c4a178d89c
knowledge91 <dirkhornung91@gmail.com>
2017-12-19 19:55:33 +0100

    translated Navigation & Home

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -13,3 +13,33 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-110991818-1', 'auto');
 ga('send', 'pageview');
+
+
+// Facebook SDK
+window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '1743051222670922',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.11'
+    });
+
+    FB.Event.subscribe('send_to_messenger', function(e) {
+        // callback for events triggered by the plugin
+        console.log(e);
+      });
+
+      FB.Event.subscribe('messaging_optins', function(e) {
+          // callback for events triggered by the plugin
+          console.log(e);
+        });
+
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
