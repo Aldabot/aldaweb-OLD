b420af8cd08be291000a38fec4da698ca3744211
knowledge91 <dirkhornung91@gmail.com>
2017-12-25 20:27:14 +0100

    added react

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,9 +1,28 @@
 import './modules';
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import Axios from 'axios';
+import $ from 'jquery';
+const uuidv4 = require('uuid/v4');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
 
 // loads the Icon plugin
 UIkit.use(Icons);
+
+// Axios({
+//   method:'get',
+//   url:'https://api.aldabot.es/v1/session',
+//   withCredentials:true
+// })
+//   .then(function(response) {
+//   console.log(response)
+// });
 
 // Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -13,6 +32,8 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-110991818-1', 'auto');
 ga('send', 'pageview');
+
+
 
 
 // Facebook SDK
@@ -27,12 +48,19 @@ window.fbAsyncInit = function() {
     FB.Event.subscribe('send_to_messenger', function(e) {
         // callback for events triggered by the plugin
         console.log(e);
+        // if messenger opt-in successfull save uuid cookie
+        // ( the uuid has also been given to the messenger webhook and is connected to the PSID )
+        if (e.event == 'opt_in') {
+          const uuid = uuidv4();
+          $(".fb-send-to-messenger").attr("data-ref", uuid);
+          document.cookie = "session="+uuid;
+        }
       });
 
-      FB.Event.subscribe('messaging_optins', function(e) {
-          // callback for events triggered by the plugin
-          console.log(e);
-        });
+      // FB.Event.subscribe('messaging_optins', function(e) {
+      //     // callback for events triggered by the plugin
+      //     console.log(e);
+      //   });
 
   };
 
