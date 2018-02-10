fbbbc90cf063278f8a9fe6eadd23b002cf525b8e
knowledge91 <dirkhornung91@gmail.com>
2018-02-10 09:05:49 +0100

    setup serverless + webpack

--- backend/handler.js
+++ backend/handler.js
@@ -1,6 +1,33 @@
 'use strict';
+import dotenv from 'dotenv';
+dotenv.config();
 
-module.exports.hello = (event, context, callback) => {
+////////////////////////////////////////////////////////////////////////////////
+// SALTEDGE
+////////////////////////////////////////////////////////////////////////////////
+
+const saltedgeClientId = process.env.SALTEDGE_CLIENT_ID;
+const saltedgeServiceSecret = process.env.SALTEDGE_SERVICE_SECRET;
+
+const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Client-id': 'QTPsSIxhOxBxIRf3IKzWew',
+    'Service-secret': 'b6aeHuRHbvQouDqS_zB-R0cdXzKdvbi3kLnkMYE6EcU'
+};
+const saltedgeApi = create({
+    baseURL: 'https://www.saltedge.com/api/v3',
+    timeout: 1000,
+    headers
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// LAMBDA HANDLER
+////////////////////////////////////////////////////////////////////////////////
+
+export const hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
